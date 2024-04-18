a63071b7f42daff63928f0b3627ba74c4a34d801
buzzdead <sigmundvolden@gmail.com>
2024-04-18 18:17:13 +0200

    mobile

--- src/spaceobjects/tools/pyramidMesh.tsx
+++ src/spaceobjects/tools/pyramidMesh.tsx
@@ -15,8 +15,8 @@ interface Props {
 }
 
 const SelectedIcon = ({color, position, fireIcon = false, handleFire}: Props) => {
-    const geometry = fireIcon ? new BoxGeometry(0.5, 1.25 , 1, 2) :  new ConeGeometry(0.25, 1, 4); // Radius, height, number of sides = 4
-    const material = new MeshPhongMaterial({ color: color });
+    const geometry = fireIcon ? new BoxGeometry(1.5, 1.25 , 4) :  new ConeGeometry(0.25, 1, 4); // Radius, height, number of sides = 4
+    const material = new MeshPhongMaterial({ color: color, opacity: fireIcon ? 0.05 : 1, transparent: fireIcon ? true : false });
     const pyramidMesh = new Mesh(geometry, material);
     pyramidMesh.position.set(position.x, position.y, position.z)
     pyramidMesh.rotation.x = 3.22
