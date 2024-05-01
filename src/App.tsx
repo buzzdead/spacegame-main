d3f22514a0f99fd738124eefe281c8d17c8381c6
buzzdead <sigmundvolden@gmail.com>
2024-05-01 09:22:00 +0200

    changed name of menu?

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./styles/theme.css";
 import AudioPlayer from "./features/AudioPlayer";
 import SpaceGame from "./startup/SpaceGame";
-import Menu from "./features/Menu";
+import Menu from "./features/menu";
 const gameMusic = require("./assets/sd.mp3");
 
 const App: React.FC = () => {
